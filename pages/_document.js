8a3afe40309f599151b79f83073e8dad6ce88bf2
bloggerHj <devcharli@gmail.com>
2025-02-10 10:34:21 -0800

    naver

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+          <meta
+            name="naver-site-verification"
+            content="3386b78475d14f0a9e10a848fd5281c43649b91d"
+          />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
